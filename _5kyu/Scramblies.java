63874bcaed79cf5264b6be0e74c42b374ee5e023
kannan-c1609 <67515928+kannan-c1609@users.noreply.github.com>
2023-01-06 14:55:08 +0530

    Add files via upload

--- _5kyu/Scramblies.java
+++ _5kyu/Scramblies.java
@@ -1,16 +1,53 @@
-package kata.kyu5;
+package main.java.kyu5;
+
+/**
+ * 5 kyu - Scramblies
+ * <p>
+ * https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+ * <p>
+ * Details:
+ * <p>
+ * Complete the function scramble(str1, str2) that returns true if a portion of str1 characters
+ * can be rearranged to match str2, otherwise returns false.
+ * <p>
+ * Notes:
+ * <p>
+ * Only lower case letters will be used (a-z). No punctuation or digits will be included.
+ * Performance needs to be considered
+ */
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Scramblies {
+
     public static boolean scramble(String str1, String str2) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : str1.toCharArray()) map.put(ch, 1 + map.getOrDefault(ch, 0));
-        for (char ch : str2.toCharArray()) {
-            Integer count = map.getOrDefault(ch, 0);
-            if (count == 0) return false;
-            map.put(ch, --count);
+        if (str2 == null || str2.length() == 0) {
+            return true;
         }
+        HashMap<Character, Integer> s1 = new HashMap<>();
+        HashMap<Character, Integer> s2 = new HashMap<>();
+
+        for (char ch : str1.toCharArray()) {
+            char current = Character.toLowerCase(ch);
+            s1.put(current, s1.getOrDefault(current, 0) + 1);
+        }
+
+        for (char ch : str2.toCharArray()) {
+            char current = Character.toLowerCase(ch);
+            s2.put(current, s2.getOrDefault(current, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : s2.entrySet()) {
+            try {
+                if (entry.getValue() > s1.get(entry.getKey())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
