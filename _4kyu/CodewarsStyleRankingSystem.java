55b24334d6c650afcd2e870521bf2f324461f688
kannan-c1609 <67515928+kannan-c1609@users.noreply.github.com>
2023-01-06 14:40:51 +0530

    Add files via upload

---
+++ _4kyu/CodewarsStyleRankingSystem.java
@@ -0,0 +1,27 @@
+package kata.kyu4;
+
+public class CodewarsStyleRankingSystem {
+
+    public static class User {
+        int rank = -8;
+        int progress = 0;
+
+        public void incProgress(int aRank) {
+            if (aRank < -8 || aRank > 8 || aRank == 0) throw new IllegalArgumentException();
+            if (this.rank < 0 && aRank > 0) aRank--;
+            if (this.rank > 0 && aRank < 0) aRank++;
+            if (this.rank == aRank) this.progress += 3;
+            if (this.rank - 1 == aRank) this.progress += 1;
+            if (this.rank - 2 >= aRank) this.progress += 0;
+            if (this.rank < aRank) {
+                int d = aRank - this.rank;
+                this.progress += (10 * d * d);
+            }
+            while (this.progress >= 100 && this.rank < 8) {
+                if (++this.rank == 0) this.rank++;
+                this.progress -= 100;
+            }
+            if (this.rank == 8) this.progress = 0;
+        }
+    }
+}
---
+++ _4kyu/SquareIntoSquaresProtectTrees.java
@@ -0,0 +1,46 @@
+package kata.kyu4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SquareIntoSquaresProtectTrees {
+
+    public String decompose(long n) {
+
+        int index;
+        List<Long> numbers = new ArrayList<>();
+        long nSquare = (long) Math.pow(n, 2);
+        long sum = fillList(n, nSquare, 0, numbers);
+        index = numbers.size();
+        while (sum != nSquare && index > 0) {
+            n = numbers.get(--index);
+            numbers = numbers.subList(0, index);
+            sum = getSum(numbers);
+            sum = fillList(n, nSquare, sum, numbers);
+            if (index == 0) index = numbers.size();
+        }
+        return (getSum(numbers) == nSquare) ? numbers.stream().sorted().map(Object::toString).collect(Collectors.joining(" ")) : null;
+    }
+
+    private long fillList(long n, long nSquare, long sum, List<Long> numbers) {
+        while (sum < nSquare && --n > 0) {
+            long nextNum = nextNumber((nSquare - sum), n);
+            numbers.add(nextNum);
+            n = nextNum;
+            sum += (long) Math.pow(nextNum, 2);
+        }
+        return sum;
+    }
+
+    private long getSum(List<Long> numbers) {
+        long sum = 0;
+        for (long num : numbers) sum += (long) Math.pow(num, 2);
+        return sum;
+    }
+    private long nextNumber(long restSum, long n) {
+        long tempSum = (long) Math.pow(n, 2);
+        while (tempSum > restSum) tempSum = (long) Math.pow(--n, 2);
+        return n;
+    }
+}
