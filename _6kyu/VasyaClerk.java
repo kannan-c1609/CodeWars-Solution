cd8a9ddf54c259ab88ed285d9fbd9ef3f9e2d826
kannan-c1609 <67515928+kannan-c1609@users.noreply.github.com>
2023-01-06 14:56:29 +0530

    Add files via upload

--- _6kyu/VasyaClerk.java
+++ _6kyu/VasyaClerk.java
@@ -1,24 +1,55 @@
-package kata.kyu6;
+package main.java.kyu6;
+
+/**
+ * 6 kyu - Vasya Clerk
+ *
+ * https://www.codewars.com/kata/555615a77ebc7c2c8a0000b8
+ *
+ * Details:
+ *
+ * The new "Avengers" movie has just been released! There are a lot of people at the cinema box
+ * office standing in a huge line.
+ * Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+ *
+ * Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this
+ * line.
+ *
+ * Can Vasya sell a ticket to every person and give change if he initially has no money and sells
+ * the tickets strictly in the order people queue?
+ *
+ * Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at
+ * hand at that moment. Otherwise return NO.
+ */
 
 public class VasyaClerk {
-    public static String tickets(int[] peopleInLine) {
-        int d25 = 0;
-        int d50 = 0;
-        for (int i = 0; i < peopleInLine.length; i++) {
-            if (peopleInLine[i] == 25) d25++;
-            if (peopleInLine[i] == 50) {
-                d25--;
-                d50++;
-            }
-            if (peopleInLine[i] == 100) {
-                if (d50 > 0) {
-                    d50--;
-                    d25--;
+    public static final int PRICE = 25;
+    
+    public static String Tickets(int[] peopleInLine) {
+        int countQuarter = 0;
+        int countFifty = 0;
+
+        for (int i : peopleInLine) {
+            if (i == PRICE) {
+                countQuarter++;
+            } else if (i == 50) {
+                countQuarter--;
+
+                if (countQuarter < 0) {
+                    return "NO";
+                }
+
+                countFifty++;
+            } else if (i == 100) {
+                if (countFifty == 0) {
+                    countQuarter -= 3;
                 } else {
-                    d25 -= 3;
+                    countQuarter--;
+                    countFifty--;
+                }
+                if (countQuarter < 0 || countFifty < 0) {
+                    return "NO";
                 }
             }
-            if (d25 < 0) return "NO";
         }
         return "YES";
     }
